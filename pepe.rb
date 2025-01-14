cdd3a9b67786344897dac47bc163ed02a985a024
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2025-01-14 19:02:28 +0100

    Add Homebrew formula for pepe

--- pepe.rb
+++ pepe.rb
@@ -1,14 +1,14 @@
 class Pepe < Formula
   desc "HTTP load generator and performance testing tool"
   homepage "https://github.com/omarmhaimdat/pepe"
-  version "0.1.4"
+  version "0.1.5"
 
   if Hardware::CPU.arm?
-    url "https://pepe.mhaimdat.com/0.1.4/aarch64-apple-darwin/pepe"
-    sha256 "25169caaa8c0033fb1f8c6e0cefc644359acde220ee6997d3763c938312c161a"
+    url "https://pepe.mhaimdat.com/0.1.5/aarch64-apple-darwin/pepe"
+    sha256 "70cc5af5c12cf2b0575d89bbea5dec873b497d8b3027fd77469575a20067ee04"
   else
-    url "https://pepe.mhaimdat.com/0.1.4/x86_64-apple-darwin/pepe"
-    sha256 "b50ad27e0f41f4e308e4cd5d58e6c6d1deaf9fccbe17f8efeb75001e145d1d2b"
+    url "https://pepe.mhaimdat.com/0.1.5/x86_64-apple-darwin/pepe"
+    sha256 "c391199f9a6ff98120288e5878a05208c17ae353b49762bca0237608e05104e3"
   end
 
   def install
