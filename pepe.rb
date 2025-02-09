e67f35deb9d7b984d1e741fc94a3c182d99e4207
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2025-02-09 21:44:30 +0100

    Update Homebrew formula for pepe to version 0.2.0

--- pepe.rb
+++ pepe.rb
@@ -4,10 +4,10 @@ class Pepe < Formula
   version "0.2.0"
 
   if Hardware::CPU.arm?
-    url "https://pepe.mhaimdat.com//aarch64-apple-darwin/pepe"
+    url "https://pepe.mhaimdat.com/0.2.0/aarch64-apple-darwin/pepe"
     sha256 "f332b5289e0dee3f50c4f61e3da2b1d0b2e54cd901f867038972787220c6b0fe"
   else
-    url "https://pepe.mhaimdat.com//x86_64-apple-darwin/pepe"
+    url "https://pepe.mhaimdat.com/0.2.0/x86_64-apple-darwin/pepe"
     sha256 "4c46f0eaa7abe79f213022bd7097d11ab6155c06a74eebb41e9a94c944d4233d"
   end
 
