23792f03261fe38f223b4ddb17065d8aaf2881b8
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2025-02-12 18:27:38 +0100

    Update Homebrew formula for pepe to version 0.2.1

--- pepe.rb
+++ pepe.rb
@@ -1,14 +1,14 @@
 class Pepe < Formula
   desc "HTTP load generator and performance testing tool"
   homepage "https://github.com/omarmhaimdat/pepe"
-  version "0.2.0"
+  version "0.2.1"
 
   if Hardware::CPU.arm?
-    url "https://pepe.mhaimdat.com/0.2.0/aarch64-apple-darwin/pepe"
-    sha256 "f332b5289e0dee3f50c4f61e3da2b1d0b2e54cd901f867038972787220c6b0fe"
+    url "https://pepe.mhaimdat.com/0.2.1/aarch64-apple-darwin/pepe"
+    sha256 "08ec537fede771c7454dd2aa373fdef234513a9ba9b94a657dcf1256971ccbdc"
   else
-    url "https://pepe.mhaimdat.com/0.2.0/x86_64-apple-darwin/pepe"
-    sha256 "4c46f0eaa7abe79f213022bd7097d11ab6155c06a74eebb41e9a94c944d4233d"
+    url "https://pepe.mhaimdat.com/0.2.1/x86_64-apple-darwin/pepe"
+    sha256 "3c380c364611ff36d7378c3a421637aef2231abc7fb00f4c90a4e72d7d84744d"
   end
 
   def install
